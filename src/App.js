1cb896f7efa17d639aa839b5d17ae19c068cb03a
Jyotiswaroop Srivastav <srivastavjyotiswaroop@gmail.com>
2023-10-28 16:02:03 +0530

    Changed URL4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
